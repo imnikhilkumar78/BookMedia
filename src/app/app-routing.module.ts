dfea8f66eea4ab1b66fa838a4c5dbe3d5c7d66a7
Nikhil-Nandan <imnikhilkumar78@gmail.com>
2023-11-07 17:26:13 +0530

    Added the home component and routed it as home page, created a child nav-bar component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponentComponent } from './home-component/home-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', pathMatch:'full', redirectTo:'home'},
+  {path:'home', component:HomeComponentComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
