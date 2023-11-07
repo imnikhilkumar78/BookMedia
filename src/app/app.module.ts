ebb12902e439dc60a2371046fee9a3917db35355
Nikhil-Nandan <imnikhilkumar78@gmail.com>
2023-11-07 17:26:13 +0530

    Added the home component and routed it as home page, created a child nav-bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { NavBarComponent } from './home-component/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponentComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
